a7a0d7303ceba1a16bcecbaa0a355f75bca9f75b
priya <priyastar23897@gmail.com>
2018-07-20 11:56:35 +0530

    First commit

---
+++ src/main/java/room_detail.java
@@ -0,0 +1,55 @@
+
+
+public class room_detail {
+
+	String r_name;
+	int r_capacity;
+	boolean p_avail;
+	boolean b_avail;
+	String r_nearby;
+	String r_loc;
+	boolean r_avail;
+	public String getR_name() {
+		return r_name;
+	}
+	public void setR_name(String r_name) {
+		this.r_name = r_name;
+	}
+	public int getR_capacity() {
+		return r_capacity;
+	}
+	public void setR_capacity(int r_capacity) {
+		this.r_capacity = r_capacity;
+	}
+	public boolean isP_avail() {
+		return p_avail;
+	}
+	public void setP_avail(boolean p_avail) {
+		this.p_avail = p_avail;
+	}
+	public boolean isB_avail() {
+		return b_avail;
+	}
+	public void setB_avail(boolean b_avail) {
+		this.b_avail = b_avail;
+	}
+	public String getR_nearby() {
+		return r_nearby;
+	}
+	public void setR_nearby(String r_nearby) {
+		this.r_nearby = r_nearby;
+	}
+	public String getR_loc() {
+		return r_loc;
+	}
+	public void setR_loc(String r_loc) {
+		this.r_loc = r_loc;
+	}
+	public boolean isR_avail() {
+		return r_avail;
+	}
+	public void setR_avail(boolean r_avail) {
+		this.r_avail = r_avail;
+	}
+	
+}
---
+++ src/main/java/signdetail.java
@@ -0,0 +1,32 @@
+
+
+public class signdetail {
+	int e_id;
+	String e_name;
+	String e_email;
+	String e_password;
+	public int getE_id() {
+		return e_id;
+	}
+	public void setE_id(int e_id) {
+		this.e_id = e_id;
+	}
+	public String getE_name() {
+		return e_name;
+	}
+	public void setE_name(String e_name) {
+		this.e_name = e_name;
+	}
+	public String getE_email() {
+		return e_email;
+	}
+	public void setE_email(String e_email) {
+		this.e_email = e_email;
+	}
+	public String getE_password() {
+		return e_password;
+	}
+	public void setE_password(String e_password) {
+		this.e_password = e_password;
+	}
+}
